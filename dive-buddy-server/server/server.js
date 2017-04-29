627e6b7cf97706cc64e4516d1814e4f3209533ff
Tristan Southwell <tristan.southwell@hotmail.com>
2017-04-29 16:25:24 +1000

    this is stupid

--- dive-buddy-server/server/server.js
+++ dive-buddy-server/server/server.js
@@ -1,13 +1,45 @@
-var express = require('express');
-var _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
-var PORT = 3001;
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
 
-var app = express();
+const PORT = 3001;
+
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('it worked');
 })
+
+// SIGN IN
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  })
+  .then((token) => {
+    res.header('x-auth', token).send(user)
+  })
+  .catch((err) => {
+    res.status(400).send(err);
+  })
+});
+
+// app.get('/users/me', authenticate, (req, res) => {
+//   res.send(req.user)
+// })
+//
+// // LOG IN
+// app.get('/users/login', (req, res) => {
+//
+// })
 
 
 app.listen(PORT, function () {
